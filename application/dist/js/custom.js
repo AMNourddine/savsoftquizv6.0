a1229979a8c88103fc71f9f5cb616931d23c485e
Azure User <azureuser@example.com>
2025-09-17 10:01:54 +0000

    Update domain configuration for Azure host

--- application/dist/js/custom.js
+++ application/dist/js/custom.js
@@ -1,5 +1,6 @@
-var api_base_url="https://YOUR-DOMAINNAME/api/public/";
-var api_site_url="https://YOUR-DOMAINNAME/api/public/index.php/";
+var api_base_url="https://nour.eastus.cloudapp.azure.com/api/public/";
+var api_site_url="https://nour.eastus.cloudapp.azure.com/api/public/index.php/";
+
 // 0 for disabled 1 for enabled
 var debug_mode=0;
 var user_data;
